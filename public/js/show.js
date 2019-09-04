cf35e141cb0d7052524906021ea3755cbea5290e
Sean Ryan <seanpryan89@gmail.com>
2019-09-03 20:57:01 -0400

    adding show into element into DOM

--- public/js/show.js
+++ public/js/show.js
@@ -1,3 +1,12 @@
+const showInfo = `<div class="show-info-container">
+<h1 class="show-title">
+    <a href="">Test Show Title</a>
+</h1>
+<p class="show-description">Test show description. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque, maiores soluta eos deserunt est aspernatur dolore odio eum alias eius? Fugiat omnis cupiditate natus quia necessitatibus perspiciatis magnam aspernatur
+    id. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima officia sunt distinctio voluptas eaque ullam itaque vitae omnis beatae repellendus.</p>
+<p><span class="show-title">This show</span> first aired on <span class="first-air"></span></p>
+</div>`
+
 const getShowData = async() => {
     const url = window.location.href;
     const paramArray = url.split("/");
@@ -5,6 +14,14 @@ const getShowData = async() => {
     const showResponse = await fetch(`/show/data/${showId}`);
     const showDataJSON = await showResponse.json();
     console.log(showDataJSON);
+}
+
+const addShowData = (showDataJSON) => {
+    document.body.appendChild(showInfo);
+}
+
+const getShowTitle = (data) => {
+
 }
 
 getShowData();
